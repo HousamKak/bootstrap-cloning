7cee7b6e0a2e51ff1e3210c41c8bc469567c6d67
“Housam <housam.kak20@gmail.com>
2022-09-14 11:20:32 +0300

    connected html to js

--- Frontend/Bootstrap-cloning/index.js
+++ Frontend/Bootstrap-cloning/index.js
@@ -1,33 +1,25 @@
-const Name=document.querySelector('#Name')
+// Finding the input text elements
+const namee=document.querySelector('#name')
 const email=document.querySelector('#email')
 const phone=document.querySelector('#phone')
-const Message=document.querySelector('#message')
-// const Namebr=document.querySelector('#Name-br')
-// const emailbr=document.querySelector('#email-br')
-// const phonebr=document.querySelector('#phone-br')
-// const Messagebr=document.querySelector('#Message-br')
-// const div=document.createElement('div')
-// const p=document.createElement('p')
+const message=document.querySelector('#message')
+
 
 // fetch("put local link",{method:'POST', body: new URLSearchParams{"name":'k'})
-"hello"
 
-Name.addEventListener('input', (event) => {
-    const input = event.target.value;
-    // if (input.length<5){
-    // Namebr.after(div)
-    // div.append(p)
-    // p.append(Document.createTextNode("A name is required."))
-    // }
-
+namee.addEventListener('input', (event) => {
+    name_value = event.target.value;
+    console.log(name_value)
 })
 
 email.addEventListener('input', (event) => {
-    const  email_value= event.target.value;
+    email_value= event.target.value;
 })
 
 phone.addEventListener('input', (event) => {
-    const phone_value = event.target.value;})
+    phone_value = event.target.value;})
 
 message.addEventListener('input', (event) => {
- const message_value = event.target.value;})
+    message_value = event.target.value;})
+
+    
