abe5a8723e4779e5c28a26808bdf5b3acd16c783
“Housam <housam.kak20@gmail.com>
2022-09-14 11:47:26 +0300

    Adding btn click

--- Frontend/Bootstrap-cloning/index.js
+++ Frontend/Bootstrap-cloning/index.js
@@ -3,7 +3,8 @@ const Name=document.querySelector('#name')
 const email=document.querySelector('#email')
 const phone=document.querySelector('#phone')
 const message=document.querySelector('#message')
+const btn=document.querySelector('#mybtn')
 const mylocalmachine="http://localhost/se/bootstrap-cloning/backend/adduserinfo.php"
+btn.addEventListener(onclick,()=>
 fetch(mylocalmachine,{method:'POST', body: new URLSearchParams({"name":Name.value,"email":email.value,"phone":phone.value,"message":message.value})})
-
-    
+)
