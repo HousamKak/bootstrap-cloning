2b88e26560a8aea3cf4907ea6b2bb8be98b89773
“Housam <housam.kak20@gmail.com>
2022-09-14 10:56:44 +0300

    Established the connection with the database

--- Frontend/Bootstrap-cloning/index.js
+++ Frontend/Bootstrap-cloning/index.js
@@ -1,28 +1,33 @@
 const Name=document.querySelector('#Name')
 const email=document.querySelector('#email')
 const phone=document.querySelector('#phone')
-const Message=document.querySelector('#Message')
-const Namebr=document.querySelector('#Name-br')
-const emailbr=document.querySelector('#email-br')
-const phonebr=document.querySelector('#phone-br')
-const Messagebr=document.querySelector('#Message-br')
-const div=document.createElement('div')
-const p=document.createElement('p')
+const Message=document.querySelector('#message')
+// const Namebr=document.querySelector('#Name-br')
+// const emailbr=document.querySelector('#email-br')
+// const phonebr=document.querySelector('#phone-br')
+// const Messagebr=document.querySelector('#Message-br')
+// const div=document.createElement('div')
+// const p=document.createElement('p')
+
+// fetch("put local link",{method:'POST', body: new URLSearchParams{"name":'k'})
+"hello"
+
 Name.addEventListener('input', (event) => {
     const input = event.target.value;
-    if (input.length<5){
-    Namebr.after(div)
-    div.append(p)
-    p.append(Document.createTextNode("A name is required."))
-    }
+    // if (input.length<5){
+    // Namebr.after(div)
+    // div.append(p)
+    // p.append(Document.createTextNode("A name is required."))
+    // }
 
 })
 
 email.addEventListener('input', (event) => {
-    const input = event.target.value;})
+    const  email_value= event.target.value;
+})
 
 phone.addEventListener('input', (event) => {
-    const input = event.target.value;})
+    const phone_value = event.target.value;})
 
-Message.addEventListener('input', (event) => {
- const input = event.target.value;})
+message.addEventListener('input', (event) => {
+ const message_value = event.target.value;})
