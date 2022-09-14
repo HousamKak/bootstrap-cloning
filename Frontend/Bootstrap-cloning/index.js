5530befd3b7a39006ae6f59f1677c08ed0a49508
“Housam <housam.kak20@gmail.com>
2022-09-14 12:16:56 +0300

    Fixed the fetching

--- Frontend/Bootstrap-cloning/index.js
+++ Frontend/Bootstrap-cloning/index.js
@@ -1,10 +1,8 @@
 // Finding the input text elements
-const Name=document.querySelector('#name')
-const email=document.querySelector('#email')
-const phone=document.querySelector('#phone')
-const message=document.querySelector('#message')
-const btn=document.querySelector('#mybtn')
-const mylocalmachine="http://localhost/se/bootstrap-cloning/backend/adduserinfo.php"
-btn.addEventListener(onclick,()=>
-fetch(mylocalmachine,{method:'POST', body: new URLSearchParams({"name":Name.value,"email":email.value,"phone":phone.value,"message":message.value})})
-)
+btn=document.querySelector('#mybtn')
+Name=document.querySelector('#name')
+email=document.querySelector('#email')
+phone=document.querySelector('#phone')
+message=document.querySelector('#message')
+mylocalmachine="http://localhost/se/bootstrap-cloning/backend/adduserinfo.php"
+btn.addEventListener('click',function(){fetch(mylocalmachine,{method:'POST', body: new URLSearchParams({"name":Name.value,"email":email.value,"phone":phone.value,"message":message.value})})})
